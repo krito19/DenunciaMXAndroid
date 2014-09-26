46218712ddb2bfb3321e429576063c2658a0897c
JARP <ramirezp.jorge@gmail.com>
2014-09-26 10:11:17 -0500

    Building initial project

---
+++ app/src/main/java/com/syca/apps/gob/denunciamx/DenunciaMainFragment.java
@@ -0,0 +1,15 @@
+package com.syca.apps.gob.denunciamx;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by JARP on 9/26/14.
+ */
+public class DenunciaMainFragment extends Fragment {
+
+    public static Fragment createInstance()
+    {
+        DenunciaMainFragment fragment = new DenunciaMainFragment();
+        return fragment;
+    }
+}
---
+++ app/src/main/java/com/syca/apps/gob/denunciamx/SingleFragmentActivity.java
@@ -0,0 +1,34 @@
+package com.syca.apps.gob.denunciamx;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+
+/**
+ * Created by JARP on 9/26/14.
+ */
+public abstract class SingleFragmentActivity extends FragmentActivity {
+
+    protected abstract Fragment createFragment();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+
+        FragmentManager manager = getSupportFragmentManager();
+
+        Fragment fragment = manager.findFragmentById(R.id.fragmentContainer);
+
+        if(null!=fragment)
+        {
+            fragment = createFragment();
+
+            manager.beginTransaction()
+                    .add(R.id.fragmentContainer,fragment)
+                    .commit();
+        }
+    }
+}
