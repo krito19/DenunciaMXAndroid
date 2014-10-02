d7f73d55867e28ab816dba2b9c925a872198c2bb
JARP <ramirezp.jorge@gmail.com>
2014-10-02 12:49:05 -0500

    Adding images

--- app/src/main/java/com/syca/apps/gob/denunciamx/ui/MisDenunciasActivity.java
+++ app/src/main/java/com/syca/apps/gob/denunciamx/ui/MisDenunciasActivity.java
@@ -1,9 +1,13 @@
 package com.syca.apps.gob.denunciamx.ui;
 
-import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.Fragment;
 
 /**
  * Created by JARP on 10/1/14.
  */
-public class MisDenunciasActivity extends FragmentActivity {
+public class MisDenunciasActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return null;
+    }
 }
